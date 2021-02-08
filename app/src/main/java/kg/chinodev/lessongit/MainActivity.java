9949207380421e0fa94528ec9da6b9cd7512e443
4inodev <4ino.dev@gmail.com>
2021-02-08 18:40:27 +0600

    add btn

--- app/src/main/java/kg/chinodev/lessongit/MainActivity.java
+++ app/src/main/java/kg/chinodev/lessongit/MainActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView myTextView;
+    Button second;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myTextView = findViewById(R.id.hello_text_view);
+        second = findViewById(R.id.second_button);
     }
 }
