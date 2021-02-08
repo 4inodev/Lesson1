9ec27955e8ebb7933b27e99821bafab866d9e96f
4inodev <4ino.dev@gmail.com>
2021-02-08 19:24:28 +0600

    расчеты

--- app/src/main/java/kg/chinodev/lessongit/MainActivity.java
+++ app/src/main/java/kg/chinodev/lessongit/MainActivity.java
@@ -17,12 +17,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         second = findViewById(R.id.second_button);
 
+
+        int x  = 4;
+        int y  = x * 15;
         second.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                System.out.println("Go!");
                 Intent myIntent = new Intent(MainActivity.this, SecondActivity.class);
+                myIntent.putExtra("sdasda", "sdasdas" +  y);
                 startActivityForResult(myIntent, 75);
             }
         });
+        System.out.println("Go!");
     }
 }
