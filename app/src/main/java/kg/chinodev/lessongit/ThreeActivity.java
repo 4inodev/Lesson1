3a182cfb3f9237d7e473f698f00543a8a4a91701
Windows 10 <emirbajnazarov44@gmail.com>
2021-02-08 19:55:08 +0600

    add new activity

---
+++ app/src/main/java/kg/chinodev/lessongit/ThreeActivity.java
@@ -0,0 +1,19 @@
+package kg.chinodev.lessongit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
+public class ThreeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three);
+    }
+}
