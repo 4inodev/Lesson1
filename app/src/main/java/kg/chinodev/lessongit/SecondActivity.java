2296aaa3bfe7fb2bdc7d60492ddbe628caf03f98
ainuska1111 <baktybekova005@gmail.com>
2021-02-10 03:42:23 +0600

    add Activity

--- app/src/main/java/kg/chinodev/lessongit/SecondActivity.java
+++ app/src/main/java/kg/chinodev/lessongit/SecondActivity.java
@@ -2,13 +2,28 @@ package kg.chinodev.lessongit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    Button thirdButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        thirdButton = findViewById(R.id.third_button);
+
+        thirdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                intent.putExtra( "name", "Ainuska");
+                startActivityForResult(intent, 1111 );
+            }
+        });
     }
 }
