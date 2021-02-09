ddff6787b1563dd783cc0efb08ebba111d657f08
xiniinix <xiniinix@list.ru>
2021-02-09 22:03:13 +0300

    created DaniarActivity and added button into SecondActivity

---
+++ app/src/main/java/kg/chinodev/lessongit/DaniarActivity.java
@@ -0,0 +1,13 @@
+package kg.chinodev.lessongit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DaniarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daniar);
+    }
+}
--- app/src/main/java/kg/chinodev/lessongit/SecondActivity.java
+++ app/src/main/java/kg/chinodev/lessongit/SecondActivity.java
@@ -2,13 +2,33 @@ package kg.chinodev.lessongit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             followButton();
+
+            }
+        });
+
     }
+    public void followButton(){
+        Intent intent = new Intent(this, DaniarActivity.class);
+        startActivity(intent);
+    }
+
+
 }
