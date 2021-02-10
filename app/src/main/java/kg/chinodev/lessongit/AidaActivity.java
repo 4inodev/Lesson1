49876712ad13181669a3d5129428b92168ad2467
aidatynybekkyzy <tynybekkyzyaida@gmail.om>
2021-02-10 18:26:35 +0600

    Added Third activity AidaActivity

---
+++ app/src/main/java/kg/chinodev/lessongit/AidaActivity.java
@@ -0,0 +1,14 @@
+package kg.chinodev.lessongit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AidaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aida);
+    }
+}
--- app/src/main/java/kg/chinodev/lessongit/SecondActivity.java
+++ app/src/main/java/kg/chinodev/lessongit/SecondActivity.java
@@ -8,10 +8,21 @@ import android.view.View;
 import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
-
+Button followBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        followBtn.findViewById(R.id.followBtn);
+        followBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                follow();
+            }
+        });
+    }
+    public  void follow(){
+        Intent intent = new Intent(this,AidaActivity.class);
+        startActivity(intent);
     }
 }
